4446e37ecd5d1de84e608853e621600c4594ef16
김대린 <62937325+kimdaerin@users.noreply.github.com>
2022-07-13 23:00:13 +0900

    Update main.js
    main js 수정

--- static/js/main.js
+++ static/js/main.js
@@ -8,7 +8,6 @@ $(document).ready(function () {
 
 /* card GET 코드 */
 function show_cardList() {
-
     $.ajax({
         type: "GET",
         url: "/getpost",
@@ -18,14 +17,32 @@ function show_cardList() {
 
             let rows = response['all_post']
             for (let i = 0; i < rows.length; i++) {
-
-                let address = rows[i]['address']
-                let comment = rows[i]['comment']
+                let post_id = rows[i]["_id"]
+                // let address = rows[i]['address']
+                // let comment = rows[i]['comment']
                 let file = "../static/postimg/" + rows[i]['file']
                 let location = rows[i]['location']
                 let workout = rows[i]['workout']
 
-                let temp_html = `<div class="card">
+                if (file == "../static/postimg/") {
+                    if (workout == "헬스") {
+                        file = "../static/img/헬스장.jpg"
+                    } else if (workout == "수영") {
+                        file = "../static/img/수영.jpg"
+                    } else if (workout == "등산") {
+                        file = "../static/img/등산.jpg"
+                    } else if (workout == "클라이밍") {
+                        file = "../static/img/클라이밍.jpg"
+                    } else if (workout == "싸이클") {
+                        file = "../static/img/싸이클.jpg"
+                    } else if (workout == "기타") {
+                        file = "../static/img/걷기.jpg"
+                    } else if (workout == "크로스핏") {
+                        file = "../static/img/크로스핏.jpg"
+                    }
+                }
+
+                let temp_html = `<div class="card" id="${post_id}" onclick="openmodal('${post_id}')">
                                     <img src="${file}" class="card-img-top" onerror="this.src='../static/img/헬스장.jpg'">
                                     <div class="card-body">
                                         <h5 class="card-title" id="card_img">${workout}</h5>
@@ -34,16 +51,6 @@ function show_cardList() {
                                     </div>
                                 </div>`
                 $('.cards').append(temp_html)
-
-
-                //     let modal_html = ` <div class="photo_detail">
-                // </div>
-                // <div class="comment_detail">${comment}</div>
-                // <div class="location_detail"></div>
-                // <div class="link_detail"></div>`
-                //
-                //     $(`#modal_bg`).append(modal_html)
-
             }
 
 
@@ -51,122 +58,117 @@ function show_cardList() {
     });
 }
 
-//하트연습
-function hello()  {
-    alert("좋아요!");
+// 상세 모달창에 클릭시 세부 내용 보여주기
+function detailmodal(post_id) {
+
+    $.ajax({
+        type: "POST",
+        url: "/modal",
+        data: {post_id_give: post_id,},
+        success: function (response) {
+            // console.log(response['result']['message'])
+            let rows = response['result']['data']
+
+            let address = rows['address']
+            let comment = rows['comment']
+            let file = "../static/postimg/" + rows['file']
+            let location = rows['location']
+            let workout = rows['workout']
+
+            if (file == "../static/postimg/") {
+                    if (workout == "헬스") {
+                        file = "../static/img/헬스장.jpg"
+                    } else if (workout == "수영") {
+                        file = "../static/img/수영.jpg"
+                    } else if (workout == "등산") {
+                        file = "../static/img/등산.jpg"
+                    } else if (workout == "클라이밍") {
+                        file = "../static/img/클라이밍.jpg"
+                    } else if (workout == "싸이클") {
+                        file = "../static/img/싸이클.jpg"
+                    } else if (workout == "기타") {
+                        file = "../static/img/걷기.jpg"
+                    } else if (workout == "크로스핏") {
+                        file = "../static/img/크로스핏.jpg"
+                    }
+                }
+
+            let temp_html = `<div id="modalCard">
+                                <div id="modalbox1">
+                                    <div id="modalimgbox">
+                                        <img id="modalimg" src="${file}" onerror="this.src='../static/img/헬스장.jpg'">
+                                    </div>
+
+                                    <div id="modalinpomation">
+                                        <h5>넥스트 짐</h5>
+                                        <p>${workout}</p>
+                                        <p>${location}</p>
+                                        <p>
+                                            <a href="${address}"
+                                               target="_blank">자세한 링크</a>
+                                        </p>
+                                        <p>작성자 : 헬린이</p>
+                                        
+                                        <div id="disqus_thread"></div>
+                                        <script>
+                                            /**
+                                            *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+                                            *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
+                                            /*
+                                            var disqus_config = function () {
+                                            this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+                                            this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+                                            };
+                                            */
+                                            (function() { // DON'T EDIT BELOW THIS LINE
+                                            var d = document, s = d.createElement('script');
+                                            s.src = 'https://coding-ws8fsinfun.disqus.com/embed.js';
+                                            s.setAttribute('data-timestamp', +new Date());
+                                            (d.head || d.body).appendChild(s);
+                                            })();
+                                        </script>
+                                    </div>
+
+                                </div>
+                                <div id="modalbox2">
+                                    <p id="review">리뷰 : ${comment}</p>
+                                </div>
+                            </div>`
+            $('#modal').append(temp_html)
+        }
+    })
 }
+
+/* 모달창  */
+
+function openmodal(a) {
+    detailmodal(a)
+    $('#modal').css('display', 'block');
+}
+
+$('#modalBg').on('click', function () {
+    $('#modal').css('display', 'none');
+})
+$('#modalBtn').on('click', function () {
+    $('#modal').css('display', 'none');
+})
 
 //댓글
-//타임스템프 만들기
-function generateTime(){
-    const date = new Date();
-    const year = date.getFullYear();
-    const month = date.getMonth();
-    const wDate = date.getDate();
-    const hour = date.getHours();
-    const min = date.getMinutes();
-    const sec = date.getSeconds();
+const likeBtns = document.querySelectorAll('.likeBtn');
 
-    const time = year+'-'+month+'-'+wDate+' '+hour+':'+min+':'+sec;
-    return time;
-
+for (const likeBtn of likeBtns) {
+    likeBtn.addEventListener('click', (e) => {
+        const svgs = likeBtn.getElementsByTagName('svg');
+        console.log("clicked" + e);
+        const like = svgs[0];
+        const disLike = svgs[1];
+        if (disLike.getAttribute('display') === 'none') {
+            like.setAttribute('display', 'none');
+            disLike.setAttribute('display', 'inline-block');
+        } else {
+            like.setAttribute('display', 'inline-block');
+            disLike.setAttribute('display', 'none');
+        }
+    });
 }
 
-//유저이름 발생기
-//유저이름은 8글자로 제한.
-function generateUserName(){
-    let alphabet = 'abcdefghijklmnopqrstuvwxyz';
-    var makeUsername = '';
-    for(let i=0; i<4;i++){
-        let index = Math.floor(Math.random(10) * alphabet.length);
-        makeUsername += alphabet[index];
-    }
-    for(let j=0;j<4;j++){
-        makeUsername += "*";
-    }
-    return makeUsername;
-}
-
-function numberCount(event){
-    console.log(event.target);
-    if(event.target === voteUp){
-        console.log("2");
-      return voteUp.innerHTML++;
-
-    }else if(event.target === voteDown){
-      return voteDown.innerHTML++;
-    }
-
-}
-
-function deleteComments(event){
-    const btn = event.target;
-    const list = btn.parentNode.parentNode;//commentList
-    rootDiv.removeChild(list);
-    //메인댓글 카운트 줄이기.
-    if(mainCommentCount.innerHTML <='0'){
-        mainCommentCount.innerHTML = 0;
-    }else{
-        mainCommentCount.innerHTML--;
-    }
-}
-
-
-//댓글보여주기
-function showComment(comment){
-    const userName = document.createElement('div');
-    const inputValue = document.createElement('span');
-    const showTime = document.createElement('div');
-    const voteDiv = document.createElement('div');
-    const countSpan = document.createElement('span')
-    const voteUp = document.createElement('button');
-    const voteDown = document.createElement('button');
-    const commentList = document.createElement('div');
-    //삭제버튼 만들기
-    const delBtn = document.createElement('button');
-    delBtn.className ="deleteComment";
-    delBtn.innerHTML="삭제";
-    commentList.className = "eachComment";
-    userName.className="name";
-    inputValue.className="inputValue";
-    showTime.className="time";
-    voteDiv.className="voteDiv";
-    //유저네임가져오기
-    userName.innerHTML = generateUserName();
-    userName.appendChild(delBtn);
-    //입력값 넘기기
-    inputValue.innerText = comment;
-    //타임스템프찍기
-    showTime.innerHTML = generateTime();
-    countSpan.innerHTML=0;
-
-    //댓글뿌려주기
-    commentList.appendChild(userName);
-    commentList.appendChild(inputValue);
-    commentList.appendChild(showTime);
-    commentList.appendChild(voteDiv);
-    rootDiv.prepend(commentList);
-
-    voteUp.addEventListener("click",numberCount);
-    voteDown.addEventListener("click",numberCount);
-    delBtn.addEventListener("click",deleteComments);
-   console.dir(rootDiv);
-
-}
-
-
-
-//버튼만들기+입력값 전달
-function pressBtn(){
-   const currentVal = inputBar.value;
-
-   if(!currentVal.length){
-      alert("댓글을 입력해주세요!!");
-   }else{
-      showComment(currentVal);
-      mainCommentCount.innerHTML++;
-      inputBar.value ='';
-   }
-}
-
-btn.onclick = pressBtn;
